f245c696baf2af2ecfc011c165f5649644588525
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-05-28 17:56:49 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+
+        <form action="insert.php" method="post">
+        
+                <label>Email</label>
+                <input type="email" name="email">
+           
+            <br><br>
+                <label>Password</label>
+                <input type="password" name="password">
+                <br><br>
+            <input type="submit" name="submit" value="Submit">
+    </div>
+</body>
+
+</html>
