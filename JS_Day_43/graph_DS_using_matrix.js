530824dcaa79bc4c292b1cc0b489dda25205a585
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-04-16 09:27:45 +0530

    Add files via upload

---
+++ JS_Day_43/graph_DS_using_matrix.js
@@ -0,0 +1,12 @@
+// the path is called as edge and the node is vertex/ vertices
+// graph representation using matrix
+graph_data = [
+    [0, 1, 1, 0],
+    [1, 0, 1, 1],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0]
+]
+
+// checking whether a vertex is connected with other or not?
+console.log(graph_data[0][0]);
+console.log(graph_data[3][1]);
---
+++ JS_Day_43/graph_using_list.js
@@ -0,0 +1,14 @@
+// graph using list
+let graphData = {
+    'a': ['b', 'c'],
+    'b': ['a', 'c', 'd'],
+    'c': ['a', 'b'],
+    'd': ['b']
+};
+
+console.log(graphData.a);
+console.log(graphData.b);
+console.log(graphData.c);
+console.log(graphData.d);
+
+//console.log(graphData.a[0]);
